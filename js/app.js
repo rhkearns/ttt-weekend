03016e3ddf6bbd368599b1ce2621a8227f4eecd0
rhkearns <ryankearns217@gmail.com>
2021-10-08 11:08:20 -0400

    winConditions and start init

--- js/app.js
+++ js/app.js
@@ -1,24 +1,49 @@
 /*------------------------------ Constants --------------------------------*/
 
-
+const winConditions = [ [sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8]]
 
 
 /*-------------------------- Variables (state) ----------------------------*/
 
-let grid = [sq0, sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8]
+let grid = []
 let turn, winner
 
 
 /*---------------------- Cached Element References ------------------------*/
 
-const squares = document.querySelectorAll('.square')
+const squares = document.querySelector('.board')
 const messageEl = document.getElementById('message')
 
-
+console.dir(squares)
 
 /*--------------------------- Event Listeners -----------------------------*/
 
+squares.addEventListener('click', tester)
 
+function tester(e){
+   console.log(e.target);
+}
 
 /*------------------------------ Functions --------------------------------*/
 
+init()
+
+
+function init (){
+   grid = [null, null, null, null, null, null, null, null, null]
+   turn = 1
+   winner = null
+   
+
+}
+
+function render(){
+   grid.forEach(square){
+
+   }
+
+}
+
+function playerTurn(turn){
+
+}
