4de97e049ab0d288832ba153661acba05055f852
rhkearns <ryankearns217@gmail.com>
2021-10-08 13:48:34 -0400

    solve problem with squares

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*------------------------------ Constants --------------------------------*/
 
-const winConditions = [ [sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8]]
+const winConditions = [ [sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8], [sq0, sq4, sq8], [sq2, sq4, sq6]]
 
 
 /*-------------------------- Variables (state) ----------------------------*/
@@ -11,39 +11,68 @@ let turn, winner
 
 /*---------------------- Cached Element References ------------------------*/
 
-const squares = document.querySelector('.board')
+const squares = document.querySelectorAll('.square')
 const messageEl = document.getElementById('message')
 
 console.dir(squares)
 
 /*--------------------------- Event Listeners -----------------------------*/
 
-squares.addEventListener('click', tester)
+sq0.addEventListener('click', playerTurn)
+sq1.addEventListener('click', playerTurn)
+sq2.addEventListener('click', playerTurn)
+sq3.addEventListener('click', playerTurn)
+sq4.addEventListener('click', playerTurn)
+sq5.addEventListener('click', playerTurn)
+sq6.addEventListener('click', playerTurn)
+sq7.addEventListener('click', playerTurn)
+sq8.addEventListener('click', playerTurn)
 
-function tester(e){
+function test(e){
    console.log(e.target);
 }
-
 /*------------------------------ Functions --------------------------------*/
 
 init()
 
 
 function init (){
-   grid = [null, null, null, null, null, null, null, null, null]
+   grid = [null, 1, null, null, -1, null, null, 1, null]
    turn = 1
    winner = null
+   messageEl.innerText = "Player 1, it's your turn"
    
+   render()
 
 }
 
 function render(){
-   grid.forEach(square){
-
+   grid = [...squares]
+   for (let i = 0; i < grid.length; i++){
+      // console.log(grid[i]);
+      if (grid[i] === 1){
+         console.log(grid[i]);
+         grid[i].style.backgroundColor = "red"
+      } else if (grid[i] === -1) {
+         grid[i].style.backgroundColor = "white"
+      } else if (grid[i] = null) {
+         grid[i].style.backgroundColor = ''
+      }
    }
 
+   if (winner !== null) {
+      renderWin(winner)
+   } else playerTurn()
 }
 
 function playerTurn(turn){
+   
+}
+
+function renderWin(winner){
+
+}
+
+function winCheck(){
 
 }
