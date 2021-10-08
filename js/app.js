1b4330aeac88b9a1f9ab379f63a2269e62021609
rhkearns <ryankearns217@gmail.com>
2021-10-08 14:57:11 -0400

    player turn

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*------------------------------ Constants --------------------------------*/
 
-const winConditions = [ [sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8], [sq0, sq4, sq8], [sq2, sq4, sq6]]
+// const winConditions = [ [sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8], [sq0, sq4, sq8], [sq2, sq4, sq6]]
 
 
 /*-------------------------- Variables (state) ----------------------------*/
@@ -13,21 +13,21 @@ let turn, winner
 
 const squares = document.querySelectorAll('.square')
 const messageEl = document.getElementById('message')
-
-console.dir(squares)
+const board = document.querySelector('.board')
+console.dir(board)
 
 /*--------------------------- Event Listeners -----------------------------*/
 
-sq0.addEventListener('click', playerTurn)
-sq1.addEventListener('click', playerTurn)
-sq2.addEventListener('click', playerTurn)
-sq3.addEventListener('click', playerTurn)
-sq4.addEventListener('click', playerTurn)
-sq5.addEventListener('click', playerTurn)
-sq6.addEventListener('click', playerTurn)
-sq7.addEventListener('click', playerTurn)
-sq8.addEventListener('click', playerTurn)
-
+// sq0.addEventListener('click', playerTurn)
+// sq1.addEventListener('click', playerTurn)
+// sq2.addEventListener('click', playerTurn)
+// sq3.addEventListener('click', playerTurn)
+// sq4.addEventListener('click', playerTurn)
+// sq5.addEventListener('click', playerTurn)
+// sq6.addEventListener('click', playerTurn)
+// sq7.addEventListener('click', playerTurn)
+// sq8.addEventListener('click', playerTurn)
+board.addEventListener('click', playerTurn)
 function test(e){
    console.log(e.target);
 }
@@ -47,16 +47,17 @@ function init (){
 }
 
 function render(){
-   grid = [...squares]
-   for (let i = 0; i < grid.length; i++){
-      // console.log(grid[i]);
-      if (grid[i] === 1){
-         console.log(grid[i]);
-         grid[i].style.backgroundColor = "red"
-      } else if (grid[i] === -1) {
-         grid[i].style.backgroundColor = "white"
-      } else if (grid[i] = null) {
-         grid[i].style.backgroundColor = ''
+
+   for (let i = 0; i < squares.length; i++){
+      if (squares[i].innerText === "X"){
+         grid[i] = 1
+         squares[i].style.backgroundColor = "red"
+      } else if (squares[i].innerText === "O") {
+         grid[i] = -1
+         squares[i].style.backgroundColor = "white"
+      } else if (squares[i].innerText = '') {
+         grid[i] = null
+         squares[i].style.backgroundColor = ''
       }
    }
 
@@ -65,7 +66,18 @@ function render(){
    } else playerTurn()
 }
 
-function playerTurn(turn){
+function playerTurn(event){
+   if (turn === 1){
+      event.target.innerText = "X"
+      console.dir(event.target)
+      grid[event.target.id] = 1
+   } else if (turn === -1) {
+      event.target.innerText = "O"
+      grid[event.target.id] = -1
+   }
+   turn = turn * -1
+   console.log(turn);
+   render()
    
 }
 
